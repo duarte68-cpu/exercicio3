53c2a0ed3893dc4b829148051b9ede8284ec1f9c
duarte68-cpu <jeffersonduarte2525@gmail.com>
2024-10-02 20:33:05 -0300

    Add files via upload

---
+++ aula2.py
@@ -0,0 +1,23 @@
+# soma de dois numero
+numero1 = int(input('digite um numero: '))
+numero2 = int(input('digite segundo numero: '))
+resultado = numero1 + numero2
+print(f'O resultado de {numero1} e {numero2} e {resultado} ')
+
+# nome do usuario
+nome = input('insira o primeiro nome: ')
+sobrenome = input('insira o sobrenome: ')
+print(nome, sobrenome)
+
+# calcular media de notas
+nota1 = float(input('digite primeira nota: '))
+nota2 = float(input('digite segunda nota: '))
+nota3 = float(input('digite a terçeira nota: '))
+media = (nota1 + nota2 + nota3) /3
+print(f'A media das nota {media}')
+
+# area de um retangulo
+largura = float(input('digite a largura: '))
+altura = float(input('digite a altura: '))
+print(f'A area de retangulo {largura*altura:.2f}')
+
